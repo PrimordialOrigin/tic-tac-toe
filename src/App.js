642146847c51a6214b25d807a88cfe9a7a1c60d7
Primordial <RamerafheKaone@protonmail.com>
2024-07-23 20:33:00 +0200

    feat: Added style for each components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { GameContext } from './assets/GameContext';
 
 function App() {
   const [value, setValue] = useState("X");
-  const [board, SetBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(Array(9).fill(null));
   const [winner, setWinner] = useState(null);
 
   return (
     <div className="App">
-      <GameContext.Provider value={{ value, setValue, board, SetBoard, winner, setWinner}}>
+      <GameContext.Provider value={{ value, setValue, board, setBoard, winner, setWinner}}>
+        <h1>Welcome <span>Player</span></h1>
         <Board />
+        <button className='restart'>RESTART GAME</button>
       </GameContext.Provider>
     </div>
   );
