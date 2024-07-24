e40b39b28bd10d294d1d0e03ef7ded57a7f5f73a
Primordial <RamerafheKaone@protonmail.com>
2024-07-24 15:20:00 +0200

    feat: Added functionality -- The restart game button

--- src/App.js
+++ src/App.js
@@ -8,12 +8,26 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [winner, setWinner] = useState(null);
 
+  const restartGame = () => {
+    setBoard(Array(9).fill(null));
+    setValue("X")
+  }
+  
+
   return (
     <div className="App">
       <GameContext.Provider value={{ value, setValue, board, setBoard, winner, setWinner}}>
+
         <h1>Welcome <span>Player</span></h1>
+        
         <Board />
-        <button className='restart'>RESTART GAME</button>
+        
+        <button 
+        className='restart' 
+        onClick={restartGame}>
+          RESTART GAME
+        </button>
+      
       </GameContext.Provider>
     </div>
   );
